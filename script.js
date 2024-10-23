15612adbfc60795036d88d7cb7d4fdfddfa57571
rashmikaNavod <drashmikanavod@gmail.com>
2024-10-23 21:34:22 +0530

    add jQuery code for dom manipiulation

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-const home = document.querySelector('#home');
-const customer = document.querySelector('#customer');
-const store = document.querySelector('#store');
-const orders = document.querySelector('#orders');
+const home = $("#home");
+const customer = $("#customer")
+const store = $("#store")
+const orders = $("#orders")
 
-const customer_div =  document.querySelector('#customer-div');
-const item_div =  document.querySelector('#item-div');
-const order_div =  document.querySelector('#order-div');
+const customer_div = $("#customer-div")
+const item_div =  $("#item-div")
+const order_div =  $("#order-div")
 
-const main_div =  document.querySelector('#main-div');
-
-
-home.addEventListener('click' , ()=>{
-
-    main_div.style.display = 'none';
-
+home.on('click', ()=>{
+    customer_div.hide();
+    item_div.hide();
+    order_div.hide();
 });
 
-customer.addEventListener('click' , ()=>{
-
-    customer_div.style.display = 'block';
-
+customer.on('click', ()=>{
+    customer_div.show();
+    item_div.hide();
+    order_div.hide();
 });
 
-document.addEventListener('DOMContentLoaded',handleResize);
+store.on('click', ()=>{
+    customer_div.hide();
+    item_div.show();
+    order_div.hide();
+});
+
+orders.on('click', ()=>{
+    customer_div.hide();
+    item_div.hide();
+    order_div.show();
+});
